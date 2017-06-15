911bcf6b1a23a167ed74a8f0ea128206e018d060
Smerth <merth@me.com>
2017-06-15 14:23:16 -0700

    Set up state and accessed it in the header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import Header from './Header';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header message="Naming Contest" />
-      <div>
-        ...
-      </div>
-    </div>
+// to use state or life-cycle methods you need to define a class
+// as opposed to the state-less function used in the last lesson.
+class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {test: 'this is a string'}
+  // }
 
-  );
-};
+  // Note: in the console you can access the state object with $r 
+  // or, directly with $r.state
+  state = {
+    pageHeader: 'Naming Contests'
+  };
+
+  render () {
+    return (
+      <div className="App">
+        <Header message={this.state.pageHeader} />
+        <div>
+          ...
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
