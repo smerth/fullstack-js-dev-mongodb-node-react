d3f67ada5e1a4fc9320098b70d9c2f08eadc059f
Smerth <merth@me.com>
2017-06-15 15:07:10 -0700

    Displaying a list of objects

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import ContestPreview from './ContestPreview';
 
 class App extends React.Component {
   state = {
@@ -15,7 +16,9 @@ class App extends React.Component {
       <div className="App">
         <Header message={this.state.pageHeader} />
         <div>
-          ...
+          {this.props.contests.map(contest => 
+            <ContestPreview {...contest}/>
+          )}        
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import data from './testData';
 
-console.log(data);
-
 ReactDOM.render(
   <App contests={data.contests} />,
   document.getElementById('root')
