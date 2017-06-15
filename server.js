7939d80e1ed5fb88d280afc2508880a70d8c7bff
Smerth <merth@me.com>
2017-06-14 22:43:50 -0700

    Intro to ejs templating

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
 import config from './config';
 import express from 'express';
-// import fs from 'fs';
 import apiRouter from './api';
-
 
 const server = express();
 
-// server.get('/', (req, res) => {
-//     res.send('Hello from express!');
-// });
+server.set('view engine', 'ejs');
 
-// server.get('/about.html', (req, res) => {
-//     fs.readFile('./about.html', (err, data) => {
-//         res.send(data.toString());
-//     });
-// });
+server.get('/', (req, res) => {
+    res.render('index', {
+        content: 'Hello Express and EJS!',
+        unescapedcontent: 'Hello I am <em>bold</em>'
+    });
+});
+
 server.use('/api', apiRouter);
 server.use(express.static('public'));
 
