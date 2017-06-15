b24e042509e0cd74878d66dfb9b76dc10d0a5d08
Smerth <merth@me.com>
2017-06-14 20:12:05 -0700

    code for node https and http core modules

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
-import config, { nodeEnv, logStars } from './config';
+// import https from 'https';
 
-console.log(config, nodeEnv);
+// https.get('https://www.lynda.com', res => {
+//     console.log('Response status code: ' + res.statusCode);
 
-logStars('function');
+//     res.on('data', chunk => {
+//         console.log(chunk.toString());
+//     });
+// });
+
+import http from 'http';
+
+const server = http.createServer();
+
+server.listen(8080);
+
+server.on('request', (req, res) => {
+    res.write('Hello http!\n');
+
+    setTimeout(() => {
+        res.write('I can stream\n');
+        res.end();
+    }, 3000)
+    
+
+});
