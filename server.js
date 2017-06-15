6f42bfed45ee01485e24b9e263b67aac7e7fed86
Smerth <merth@me.com>
2017-06-14 20:51:19 -0700

    built simple express server for two routes and a simple api server

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
-// import https from 'https';
+import config from './config';
+import express from 'express';
+// import fs from 'fs';
+import apiRouter from './api';
 
-// https.get('https://www.lynda.com', res => {
-//     console.log('Response status code: ' + res.statusCode);
 
-//     res.on('data', chunk => {
-//         console.log(chunk.toString());
-//     });
+const server = express();
+
+// server.get('/', (req, res) => {
+//     res.send('Hello from express!');
 // });
 
-import http from 'http';
+// server.get('/about.html', (req, res) => {
+//     fs.readFile('./about.html', (err, data) => {
+//         res.send(data.toString());
+//     });
+// });
+server.use('/api', apiRouter);
+server.use(express.static('public'));
 
-const server = http.createServer();
-
-server.listen(8080);
-
-server.on('request', (req, res) => {
-    res.write('Hello http!\n');
-
-    setTimeout(() => {
-        res.write('I can stream\n');
-        res.end();
-    }, 3000)
-    
-
+server.listen(config.port, () => {
+    console.info('Express listening on port: ' + config.port)
 });
