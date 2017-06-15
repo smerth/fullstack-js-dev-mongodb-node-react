fbffb4312c7906eb492aad03acc046641239638c
Smerth <merth@me.com>
2017-06-15 14:05:57 -0700

    Broke App and Header components into separate modules

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header message="Naming Contest" />
+      <div>
+        ...
+      </div>
+    </div>
+
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-const Header = ({message}) => {
-  return (
-    <h2 className="Header text-center">
-      {message}
-    </h2>
-  );
-};
-
-Header.propTypes = {
-  message: React.PropTypes.string
-};
-
-const App = () => {
-  return (
-    <div className="App">
-      <Header message="Naming Contest" />
-      <div>
-        ...
-      </div>
-    </div>
-
-  );
-};
+import App from './components/App';
 
 ReactDOM.render(
   <App />,
