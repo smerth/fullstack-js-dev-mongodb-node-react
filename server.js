23e82ae7a10d878374100f76da74064102eb96e4
Smerth <merth@me.com>
2017-06-16 13:04:54 -0700

    Fetching data from the server side

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ server.use(sassMiddleware({
 
 server.set('view engine', 'ejs');
 
+
+import './serverRender';
+
 server.get('/', (req, res) => {
   res.render('index', {
     content: '...'
@@ -26,6 +29,6 @@ server.get('/', (req, res) => {
 server.use('/api', apiRouter);
 server.use(express.static('public'));
 
-server.listen(config.port, () => {
+server.listen(config.port, config.host, () => {
   console.info('Express listening on port', config.port);
 });
